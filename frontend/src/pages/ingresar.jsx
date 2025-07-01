09fc74db925e5919ceae79ae8c186899bc8e8a37
j3r0nimo <jerobalort@gmail.com>
2025-07-01 13:02:53 -0300

    1/7
    añado verificacion de dni no existente

--- frontend/src/pages/ingresar.jsx
+++ frontend/src/pages/ingresar.jsx
@@ -9,7 +9,7 @@ function Compra() {
 
   const validarFormulario = () => {
     const nuevosErrores = {};
-    
+
     if (!dni.match(/^\d{7,8}$/)) nuevosErrores.dni = "DNI inválido (7 u 8 dígitos)";
 
     setErrores(nuevosErrores);
@@ -25,30 +25,34 @@ function Compra() {
 
       if (respuesta.status === 200) {
         const data = respuesta.data;
-        
+
         localStorage.setItem("usuario", JSON.stringify(data))
 
         navigate(`/perfil`);
 
       }
     } catch (error) {
-      console.error('Error al buscar usuario:', error);
+      if (error.response?.status === 404) {
+        setErrores({ dni: "DNI no encontrado. Regístrese." });
+      } else {
+        console.log("Hubo un problema al buscar el usuario.");
+      }
     }
   };
 
   return (
     <main>
       <form>
-      <label>Ingrese su DNI:</label><br />
-      <input type="text" value={dni} onChange={(e) => setdni(e.target.value)}/>
-      
-      
-      <button type="submit" onClick={handleSubmit}>Buscar</button><br />
-      {errores.dni && <div style={{ color: 'red' }}>{errores.dni}</div>} 
-      <a href='/registrar'><b>si no tiene una cuenta haga click aca</b></a>
-    </form>
+        <label>Ingrese su DNI:</label><br />
+        <input type="text" value={dni} onChange={(e) => setdni(e.target.value)} />
+
+
+        <button type="submit" onClick={handleSubmit}>Buscar</button><br />
+        {errores.dni && <div style={{ color: 'red' }}>{errores.dni}</div>}
+        <a href='/registrar'><b>si no tiene una cuenta haga click aca</b></a>
+      </form>
     </main>
-    
+
   );
 }
 
